243406433eb8962fdff635d0657b1afe0c3f3a6c
katellrz <karaze05@gmail.com>
2024-12-14 14:13:46 +0100

    Fin de création omnicient

--- src/Omnicient.java
+++ src/Omnicient.java
@@ -1,12 +1,43 @@
-import map.Point;
-import java.util.Map;
-import entites.Entite;
+import map.Case;
+
+import java.util.LinkedList;
+import java.util.List;
+import entites.Enemi;
+import entites.Tours;
+
+public abstract class Omnicient {
+
+    public List<Enemi> positionMonstre = new LinkedList<>();
+    public List<Tours> positionTours = new LinkedList<>();
+
+    public Case Spawn;
+    public Case Base;
+
+    public List<Case> Chemin;
 
 
 
-public class Omnicient {
 
-    public Map<Point,Entite> positionMonstre;
+    public void SavetoOmni (List<Case> chemin){
+        this.Chemin=chemin;
+    }
+
+    public void SavetoOmni (Enemi e){
+        this.positionMonstre.add(e);
+    }
+
+    public void SavetoOmni (Tours e){
+        this.positionTours.add(e);
+    }
+
+    public void SavetoOmniBase (Case base){
+        this.Base=base;
+    }
+
+    public void SavetoOmniSpawn (Case spawn){
+        this.Spawn=spawn;
+    }
+
     
 
     
